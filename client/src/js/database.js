2b92d732cdc37dcfd559955e3493141412a7b2c9
Krystal Richardson <krystal.williams2019@gmail.com>
2023-08-24 18:35:30 -0400

    fixed errors

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,27 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (id, content) => {
+  console.log('PUT to the database');
+  const todosDb = await openDB('jate', 1);
+  const tx = todosDb.transaction('jate', 'readwrite');
+  const store = tx.objectStore('content');
+  const request = store.put({ id: id, content: content });
+  const result = await request;
+  console.log('Data has been saved to the database', result);
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  console.log('GET from the database');
+  const indexedDb = await openDB('jate', 1);
+  const tx = indexedDb.transaction ('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const request = store.get(1);
+  const result = await request;
+
+  console.log('result.vale', result);
+  return result;
+};
 
 initdb();
